b145b59bf6b46be19c040e430389c41578a0aaa1
Ashraf Mehdaly <developer@outlook.sa>
2017-03-22 23:01:12 +0200

    Not working version

---
+++ TheWorld_Init/wwwroot/js/site.js
@@ -0,0 +1,34 @@
+﻿//site.js
+(function () {
+
+
+    var e = $("#username");
+    e.text("New jQuery");
+
+    var main1 = $("#main");
+
+    main1.on("mouseenter",
+        function () {
+            main1.style.backgroundColor = "#222";
+        });
+
+    main1.on("mouseleave",
+        function () {
+            main1.style.backgroundColor = "";
+        });
+
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+    $("#sidebarToggleButtonButton").on("click",
+        function () {
+            $sidebarAndWrapper.toggleClass("hide-sidebar");
+            if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+                $(this).text("Show Sidebar");
+            } else {
+                $(this).text("Hide Sidebar");
+
+            }
+        });
+
+
+})();
