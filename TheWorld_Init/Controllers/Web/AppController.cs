879450bb3005f3a20176e2f6d352a7faccf32646
Ashraf Mehdaly <developer@outlook.sa>
2017-03-23 17:41:36 +0200

    Adding layout page

--- TheWorld_Init/Controllers/Web/AppController.cs
+++ TheWorld_Init/Controllers/Web/AppController.cs
@@ -7,11 +7,23 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace TheWorld_Init.Controllers.Web
 {
+    //Install-Package Microsoft.AspNetCore.Mvc
     public class AppController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
     }
 }
