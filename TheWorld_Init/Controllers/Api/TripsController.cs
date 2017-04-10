6649ca438c11fcddc5fe5e28a7a783a203831aac
Ashraf Mehdaly <developer@outlook.sa>
2017-04-10 21:31:48 +0200

    Adding Api controller

---
+++ TheWorld_Init/Controllers/Api/TripsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TheWorld_Init.Models;
+
+namespace TheWorld_Init.Controllers.Api
+{
+    public class TripsController : Controller
+    {
+        public JsonResult Get()
+        {
+            return Json(new Trip() {Name = "My Trip"});
+        }
+    }
+}
